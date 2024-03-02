d8ab497ca11e90e72e027e3b0edb9e9e9a05acb9
Giovanni De Giorgio <giovannidegiorgio1999@gmail.com>
2024-03-02 21:11:47 +0100

    feat: implement client

--- cmd/main.go
+++ cmd/main.go
@@ -1,5 +1,39 @@
 package main
 
-func main() {
+import (
+	"context"
+	"flag"
+	"fmt"
+	"github.com/gdegiorgio/grpc-bookshelf-client/internal/proto/book"
+	"github.com/rs/zerolog"
+	"github.com/rs/zerolog/log"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
+	"os"
+)
 
+var (
+	host   = flag.String("host", "localhost", "Set the GRPC Server host")
+	port   = flag.String("port", "4500", "Set the GRPC Server port")
+	pretty = flag.Bool("pretty", false, "Prettify logs")
+)
+
+func main() {
+	flag.Parse()
+	if *pretty {
+		log.Logger = log.Output(zerolog.ConsoleWriter{Out: os.Stderr})
+	}
+	var opts []grpc.DialOption
+	opts = append(opts, grpc.WithTransportCredentials(insecure.NewCredentials()))
+	conn, err := grpc.Dial(fmt.Sprintf("%s:%s", *host, *port), opts...)
+	if err != nil {
+		log.Fatal().Msg(fmt.Sprintf("Failed to connect to grpc server %s:%s - %+v", *host, *port, err))
+	}
+	defer conn.Close()
+	client := book.NewBookClient(conn)
+	book, err := client.GetBook(context.Background(), &book.GetBookRequest{Id: "1"})
+	if err != nil {
+		log.Fatal().Msg(fmt.Sprintf("Failed to retrieve book info - %+v", err))
+	}
+	log.Info().Msg(fmt.Sprintf("Got a new book %+v", book))
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,19 @@
 module github.com/gdegiorgio/grpc-bookshelf-client
 
 go 1.22.0
+
+require (
+	github.com/rs/zerolog v1.32.0
+	google.golang.org/grpc v1.62.0
+	google.golang.org/protobuf v1.32.0
+)
+
+require (
+	github.com/golang/protobuf v1.5.3 // indirect
+	github.com/mattn/go-colorable v0.1.13 // indirect
+	github.com/mattn/go-isatty v0.0.19 // indirect
+	golang.org/x/net v0.20.0 // indirect
+	golang.org/x/sys v0.16.0 // indirect
+	golang.org/x/text v0.14.0 // indirect
+	google.golang.org/genproto/googleapis/rpc v0.0.0-20240123012728-ef4313101c80 // indirect
+)
